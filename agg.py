d4875444300fa6cf753ae497f16173002748dc61
furtadobb <bernardo.furtado@ipea.gov.br>
2017-02-15 16:48:22 -0200

    Example of plotting a lot of bar graphs

---
+++ agg.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+agro = ["Arroz",	"Trigo",	"Cereais",	"Vegetais e Frutas",	"Oleaginosas",	"Cana",	"Algodão",	"Lavouras",
+        "Bovinos",	"Out. animais",	"Leite cru",	"Lã",	"Florestal",	"Pesca"]
+
+extrativa = ["Carvão",	"Petróleo",	"Gás",	"Extração"]
+
+industria = ["Ben. De arroz",	"Carne",	"Outros carne",	"Fab. Óleos",	"Lacticínios",	"Açúcar",
+             "Fab. Alimentos",	"Fab. Bebidas",	"Texteis",	"Vestuário",	"Couros",	"Madeira",	"Celulose",
+             "Refino e comb.",	"Quím. Plasticos",	"Não-metálicos",	"Metalurgia",	"Não-ferrosos",	"Prod. Metal",
+             "Veículos",	"Out. transp.",	"Máq e elétricos",	"Máq e equip.",	"Diversos"]
+
+servicos = ["Energia elét.",	"Comb. Gasosos",	"Água e esgoto",	"Construção",	"Comércio",	"Transporte",
+            "Aquaviário",	"Aéreo",	"Inf. E Com.",	"Financeiro",	"Seguros",	"Serviços empresas",
+            "Artes, esporte",	"Públicos",	"Imobiliários"]
